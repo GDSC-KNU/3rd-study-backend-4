62d85d1756e285b8e5a99600b80497e8de7c0d6a
chaejm55 <chaejm55@gmail.com>
2023-10-06 00:18:28 +0900

    2주차 4-5챕터

---
+++ src/main/java/me/chaejungmin/springbootdeveloper/MemberService.java
@@ -0,0 +1,25 @@
+package me.chaejungmin.springbootdeveloper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    Long id = 1L;
+
+    public void test() { // 생성
+        memberRepository.save(new Member(id, "A"));
+        // 조회
+        Optional<Member> member = memberRepository.findById(id);
+        List<Member> allMembers = memberRepository.findAll();
+
+        // 삭제
+        memberRepository.deleteById(id);
+    }
+}
